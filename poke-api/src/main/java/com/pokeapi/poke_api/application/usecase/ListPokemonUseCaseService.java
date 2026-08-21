fed0fe91363652b5cd140392c2d33f734083f85a
Sergio Cano <canosergio90@gmail.com>
2026-08-21 19:22:47 -0300

    formatting

--- poke-api/src/main/java/com/pokeapi/poke_api/application/usecase/ListPokemonUseCaseService.java
+++ poke-api/src/main/java/com/pokeapi/poke_api/application/usecase/ListPokemonUseCaseService.java
@@ -7,8 +7,6 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 @RequiredArgsConstructor
 public class ListPokemonUseCaseService implements ListPokemonUseCase {
--- poke-api/src/test/java/com/pokeapi/poke_api/adapter/in/web/PokemonControllerTest.java
+++ poke-api/src/test/java/com/pokeapi/poke_api/adapter/in/web/PokemonControllerTest.java
@@ -19,16 +19,6 @@ import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.List;
-
-import static org.hamcrest.Matchers.containsInAnyOrder;
-import static org.hamcrest.Matchers.hasSize;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 @WebMvcTest(PokemonController.class)
 class PokemonControllerTest {
 
@@ -47,28 +37,35 @@ class PokemonControllerTest {
     @Test
     void shouldReturnPaginatedPokemons() throws Exception {
         when(listPokemonUseCase.listPokemons(2, 10)).thenReturn(PokemonFixtures.pokemonList());
-        when(pokemonMapper.mapToPokemonSummaryDto(any())).thenReturn(
-                new PokemonSummaryDto(
-                        1,
-                        "bulbasaur",
-                        List.of("grass", "poison"),
-                        List.of("swords-dance", "razor-wind"),
-                        List.of("https://someurl/sprites/pokemon/back/1.png", "https://someurl/sprites/pokemon/front/2.png")),
-                new PokemonSummaryDto(
-                        4,
-                        "charmander",
-                        List.of("fire"),
-                        List.of("fire-punch"),
-                        List.of("https://someurl/sprites/pokemon/back/1.png", "https://someurl/sprites/pokemon/front/2.png")),
-                new PokemonSummaryDto(
-                        1,
-                        "squirtle",
-                        List.of("water"),
-                        List.of("bubble", "aqua-tail"),
-                        List.of("https://someurl/sprites/pokemon/back/1.png", "https://someurl/sprites/pokemon/front/2.png"))
-        );
+        when(pokemonMapper.mapToPokemonSummaryDto(any()))
+                .thenReturn(
+                        new PokemonSummaryDto(
+                                1,
+                                "bulbasaur",
+                                List.of("grass", "poison"),
+                                List.of("swords-dance", "razor-wind"),
+                                List.of(
+                                        "https://someurl/sprites/pokemon/back/1.png",
+                                        "https://someurl/sprites/pokemon/front/2.png")),
+                        new PokemonSummaryDto(
+                                4,
+                                "charmander",
+                                List.of("fire"),
+                                List.of("fire-punch"),
+                                List.of(
+                                        "https://someurl/sprites/pokemon/back/1.png",
+                                        "https://someurl/sprites/pokemon/front/2.png")),
+                        new PokemonSummaryDto(
+                                1,
+                                "squirtle",
+                                List.of("water"),
+                                List.of("bubble", "aqua-tail"),
+                                List.of(
+                                        "https://someurl/sprites/pokemon/back/1.png",
+                                        "https://someurl/sprites/pokemon/front/2.png")));
 
-        mockMvc.perform(get("/pokemons?page=2&size=10")).andExpect(status().isOk())
+        mockMvc.perform(get("/pokemons?page=2&size=10"))
+                .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(3)))
                 .andExpect(jsonPath("$[0].name").value("bulbasaur"))
                 .andExpect(jsonPath("$[0].category", containsInAnyOrder("poison", "grass")))
